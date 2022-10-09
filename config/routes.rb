9f433156273bc84937ec40263f55346ae92f6a6d
emanuel caro <darkemlord@gmail.com>
2022-10-09 15:06:11 +0900

    authentication working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, param: :_username
+  post '/auth/login', to: 'authentications#login'
+  get '/*a', to: 'application#not_found'
   namespace :v1 do
     resources :drinks
   end
